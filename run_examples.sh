89261bef4ee846b9681b2df68819c8b257f9e863
Pawan Goyal <pkgpawan@gmail.com>
2024-11-17 21:57:23 +0100

    fix: Data path and run_examples.sh

--- run_examples.sh
+++ run_examples.sh
@@ -6,27 +6,27 @@
 
 python --version 
 
-EPOCHS=120
+EPOCHS=12000
 cd Examples/
 
-# echo "##################################################################"
-# echo "############# Running Lorenz Example               ##############"
-# echo "##################################################################"
+echo "##################################################################"
+echo "############# Running Lorenz Example               ##############"
+echo "##################################################################"
 
-# python Example_Lorenz.py --model_hypothesis no_hypos --epochs $EPOCHS
-# echo ""
-# python Example_Lorenz.py --model_hypothesis globalstability --epochs $EPOCHS
-# echo ""
+python Example_Lorenz.py --model_hypothesis no_hypos --epochs $EPOCHS
+echo ""
+python Example_Lorenz.py --model_hypothesis globalstability --epochs $EPOCHS
+echo ""
 
-# echo "##################################################################"
-# echo "############# Running MHD Example               ##############"
-# echo "##################################################################"
+echo "##################################################################"
+echo "############# Running MHD Example               ##############"
+echo "##################################################################"
 
 
-# python Example_MHD.py --model_hypothesis no_hypos --epochs $EPOCHS
-# echo ""
-# python Example_MHD.py --model_hypothesis globalstability --epochs $EPOCHS
-# echo ""
+python Example_MHD.py --model_hypothesis no_hypos --epochs $EPOCHS
+echo ""
+python Example_MHD.py --model_hypothesis globalstability --epochs $EPOCHS
+echo ""
 
 echo "##################################################################"
 echo "############# Running Chafee Example               ##############"
@@ -41,43 +41,43 @@ echo ""
 
 # jupyter nbconvert --execute --to notebook --inplace plotting/Plotting_Chafee.ipynb
 
-# echo "##################################################################"
-# echo "############# Running Burgers Example with Dirichlet #############"
-# echo "##################################################################"
+echo "##################################################################"
+echo "############# Running Burgers Example with Dirichlet #############"
+echo "##################################################################"
 
-# python Example_Burgers_Dirichilet_Stability_order.py --model_hypothesis no_hypos --epochs $EPOCHS > ./Results/burgers_dirichlet_order_test.txt
-# echo ""
-# python Example_Burgers_Dirichilet_Stability_order.py --model_hypothesis localstability --epochs $EPOCHS >> ./Results/burgers_dirichlet_order_test.txt
-# echo ""
-# python Example_Burgers_Dirichilet_Stability_order.py --model_hypothesis globalstability --epochs $EPOCHS >> ./Results/burgers_dirichlet_order_test.txt
-# echo ""
+python Example_Burgers_Dirichilet_Stability_order.py --model_hypothesis no_hypos --epochs $EPOCHS > ./Results/burgers_dirichlet_order_test.txt
+echo ""
+python Example_Burgers_Dirichilet_Stability_order.py --model_hypothesis localstability --epochs $EPOCHS >> ./Results/burgers_dirichlet_order_test.txt
+echo ""
+python Example_Burgers_Dirichilet_Stability_order.py --model_hypothesis globalstability --epochs $EPOCHS >> ./Results/burgers_dirichlet_order_test.txt
+echo ""
 
-# python Example_Burgers_Dirichilet_Stability_reg.py --model_hypothesis no_hypos --epochs $EPOCHS > ./Results/burgers_dirichlet_reg_test.txt
-# echo ""
-# python Example_Burgers_Dirichilet_Stability_reg.py --model_hypothesis localstability --epochs $EPOCHS >> ./Results/burgers_dirichlet_reg_test.txt
-# echo ""
-# python Example_Burgers_Dirichilet_Stability_reg.py --model_hypothesis globalstability --epochs $EPOCHS >> ./Results/burgers_dirichlet_reg_test.txt
-# echo ""
+python Example_Burgers_Dirichilet_Stability_reg.py --model_hypothesis no_hypos --epochs $EPOCHS > ./Results/burgers_dirichlet_reg_test.txt
+echo ""
+python Example_Burgers_Dirichilet_Stability_reg.py --model_hypothesis localstability --epochs $EPOCHS >> ./Results/burgers_dirichlet_reg_test.txt
+echo ""
+python Example_Burgers_Dirichilet_Stability_reg.py --model_hypothesis globalstability --epochs $EPOCHS >> ./Results/burgers_dirichlet_reg_test.txt
+echo ""
 
 # jupyter nbconvert --execute --to notebook --inplace Plotting_Burgers_Dirichilet_orders.ipynb
 # jupyter nbconvert --execute --to notebook --inplace Plotting_Burgers_Dirichilet_reg.ipynb
 
-# echo "##################################################################"
-# echo "############# Running Burgers Example with Neumann   #############"
-# echo "##################################################################"
-# python Example_Burgers_Neumann_Stability_order.py --model_hypothesis no_hypos --epochs $EPOCHS > ./Results/burgers_neumann_order_test.txt
-# echo ""
-# python Example_Burgers_Neumann_Stability_order.py --model_hypothesis localstability --epochs $EPOCHS >> ./Results/burgers_neumann_order_test.txt
-# echo ""
-# python Example_Burgers_Neumann_Stability_order.py --model_hypothesis globalstability --epochs $EPOCHS >> ./Results/burgers_neumann_order_test.txt
-# echo ""
+echo "##################################################################"
+echo "############# Running Burgers Example with Neumann   #############"
+echo "##################################################################"
+python Example_Burgers_Neumann_Stability_order.py --model_hypothesis no_hypos --epochs $EPOCHS > ./Results/burgers_neumann_order_test.txt
+echo ""
+python Example_Burgers_Neumann_Stability_order.py --model_hypothesis localstability --epochs $EPOCHS >> ./Results/burgers_neumann_order_test.txt
+echo ""
+python Example_Burgers_Neumann_Stability_order.py --model_hypothesis globalstability --epochs $EPOCHS >> ./Results/burgers_neumann_order_test.txt
+echo ""
 
-# python Example_Burgers_Neumann_Stability_reg.py --model_hypothesis no_hypos --epochs $EPOCHS > ./Results/burgers_neumann_reg_test.txt
-# echo ""
-# python Example_Burgers_Neumann_Stability_reg.py --model_hypothesis localstability --epochs $EPOCHS >> ./Results/burgers_neumann_reg_test.txt
-# echo ""
-# python Example_Burgers_Neumann_Stability_reg.py --model_hypothesis globalstability --epochs $EPOCHS >> ./Results/burgers_neumann_reg_test.txt
-# echo ""
+python Example_Burgers_Neumann_Stability_reg.py --model_hypothesis no_hypos --epochs $EPOCHS > ./Results/burgers_neumann_reg_test.txt
+echo ""
+python Example_Burgers_Neumann_Stability_reg.py --model_hypothesis localstability --epochs $EPOCHS >> ./Results/burgers_neumann_reg_test.txt
+echo ""
+python Example_Burgers_Neumann_Stability_reg.py --model_hypothesis globalstability --epochs $EPOCHS >> ./Results/burgers_neumann_reg_test.txt
+echo ""
 
 # jupyter nbconvert --execute --to notebook --inplace Plotting_Burgers_Neumann_orders.ipynb
 
